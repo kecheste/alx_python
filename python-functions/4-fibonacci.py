4ce87c86157b6dca022a737ff5f002a4ff9135ca
kecheste <aben.ezer443@gmail.com>
2023-07-20 08:18:48 -0700

    Adding fixes

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,15 +1,12 @@
 def fibonacci_sequence(n):
-    n1, n2 = 0, 1
-    i = 0
-
     if n <= 0:
-        return "Enter positive integer"
-    elif n == 1:
-        return n1
-    else:
-        while i < n:
-            print(n1)
-            nth = n1 + n2
-            n1 = n2
-            n2 = nth
-            i += 1
+        return []
+    
+    fibonacci_numbers = []
+    a, b = 0, 1
+
+    for i in range(n):
+        fibonacci_numbers.append(a)
+        a, b = b, a + b
+        
+    return fibonacci_numbers
--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,7 +1,7 @@
 def is_prime(number):
-    flag = True
-    for i in range(2, number):
-        if (number % i) == 0:
-            flag = False
-            break
-    return flag
+    if number <= 1:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
