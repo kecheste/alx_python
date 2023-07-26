784cc3746cafa7f6ddbef20593f84aa6c0ff97bf
kecheste <aben.ezer443@gmail.com>
2023-07-26 11:11:25 +0300

    no c

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,5 +1,11 @@
 def no_c(my_string):
-    for c in my_string:
-        if c == 'c' or c == 'C':
-            c = ''
-        print(c, end="")
+#     new_string = my_string.translate({ord(i): None for i in 'cC'})
+#     return new_string
+#     OR
+    new_str = ''
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_str += char
+    return new_str
+
+print(no_c('Holberton School'))
