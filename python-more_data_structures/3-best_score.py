c5dad8b2fba00ad66ecc3c800adfd2a3f02e79ae
kecheste <aben.ezer443@gmail.com>
2023-07-26 15:17:50 +0300

    Finding a key having maximum value

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,11 +1,3 @@
 def best_score(a_dictionary):
-    if a_dictionary == {}:
-        return None
-    else:
-        max_key = next(iter(a_dictionary))
-        for key in a_dictionary:
-            if key is None:
-                return None
-            if a_dictionary[key] >= a_dictionary[max_key]:
-                max_key = key
-    return max_key
+    if a_dictionary:
+        return max(a_dictionary, key=a_dictionary.get)
