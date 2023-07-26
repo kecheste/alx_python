fc7e8c64fad1d59ca7ae8c1053feb66d1169e581
kecheste <aben.ezer443@gmail.com>
2023-07-26 11:50:50 +0300

    no c

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,14 +1,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col in row:
-            print("{}".format(col), end=" " if col != row[-1] else "")
-        print('$')
-
-    # OR
-
-    # for e in matrix:
-    #     for i in e:
-    #         print('{:d}'.format(i), end='')
-    #         if i != e[-1]:
-    #             print(' ', end='')
-    #     print("")
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
+        print()
