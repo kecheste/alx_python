d0a0d97c3353480a4ceea0f15d1fb167dae961b6
kecheste <aben.ezer443@gmail.com>
2023-07-26 14:39:36 +0300

    updating dictionary

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,5 +1,3 @@
 def update_dictionary(a_dictionary, key, value):
-    dict(a_dictionary)
-    a_dictionary |= {key : value}
-    for el in a_dictionary:
-        print("{}: {}".format(el,a_dictionary.get(el)))
+    a_dictionary[key] = value
+    return (a_dictionary)
