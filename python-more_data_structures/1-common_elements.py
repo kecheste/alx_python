474483fa32eb9b52f3bface1e0bd0f6cad4b2cb3
kecheste <aben.ezer443@gmail.com>
2023-07-26 14:16:20 +0300

    common elements in two sets

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,4 +1,6 @@
 def common_elements(set_1, set_2):
-    for el in set_1:
-        if el in set_2:
-            return el
+    comm = []
+    for x in set_1:
+        if x in set_2:
+            comm.append(x)
+    return comm
