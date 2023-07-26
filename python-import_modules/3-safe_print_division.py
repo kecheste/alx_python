fdf0f4cfa25c3740c7c426a9606c4a57a0e5f599
kecheste <aben.ezer443@gmail.com>
2023-07-26 10:03:13 +0300

    importing a variable

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,9 +1,8 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-        print("Inside result: {}".format(result))
-        print("{:d} / {:d} = {}".format(a, b, result))
     except:
         result = None
-        print("Inside result: {}".format(result))
-        print("{:d} / {:d} = {}".format(a, b, result))
+    finally:
+        print("Inside result: {}".format(result) )
+    return result
