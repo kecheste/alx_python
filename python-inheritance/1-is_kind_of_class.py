01bfc5e5f8730689785603b25891730a1e2d5aaa
kecheste <aben.ezer443@gmail.com>
2023-08-03 21:17:53 +0300

    initial commit

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def is_kind_of_class(obj, a_class):
     return isinstance(obj, a_class) or isinstance(type(obj), a_class)
+    __doc__ = """
+        this is documentation for my module
+        """
 
 
 __doc__ = """
