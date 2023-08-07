b04376f91f06a1d9a16113c4865208179c0e22a3
kecheste <aben.ezer443@gmail.com>
2023-08-07 15:44:51 +0300

    Rectangle model

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -80,3 +80,12 @@ class Rectangle(Base):
         """ returns the # representation of the rectangle object """
         for i in range(self.height):
             print(self.width * '#')
+
+    def __str__(self):
+        """ str special method """
+        str_rectangle = "[Rectangle] "
+        str_id = "({}) ".format(self.id)
+        str_xy = "{}/{} - ".format(self.x, self.y)
+        str_wh = "{}/{}".format(self.width, self.height)
+
+        return str_rectangle + str_id + str_xy + str_wh
