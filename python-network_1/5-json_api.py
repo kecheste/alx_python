a201f5a275fba6b292bf34f473035c51304dcfdd
kecheste <aben.ezer443@gmail.com>
2023-08-08 23:44:00 +0300

    github api

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -1,22 +1,30 @@
-#!/usr/bin/env python3
 """
 Sends a request to a URL and displays the value of the X-Request-Id header in the response.
 """
 import requests
 import sys
 
-if __name__ == "__main__":
-    letter = sys.argv[1] if len(sys.argv) > 1 else ""
 
-    payload = {"q": letter}
-    response = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+def search_user(letter):
+    """Sends a POST request to http://0.0.0.0:5000/search_user with the given letter."""
+    url = 'http://0.0.0.0:5000/search_user'
+    data = {'q': letter}
+    response = requests.post(url, data=data)
 
-    try:
-        json_response = response.json()
-        if json_response:
-            print("[{}] {}".format(json_response.get(
-                "id"), json_response.get("name")))
+    if response.status_code == 200:
+        response_json = response.json()
+        if response_json:
+            print('[{id}] {name}'.format(
+                id=response_json['id'], name=response_json['name']))
         else:
-            print("No result")
-    except ValueError:
-        print("Not a valid JSON")
+            print('No result')
+    else:
+        if response.headers['Content-Type'] == 'application/json':
+            print('Not a valid JSON')
+        else:
+            print('Error: {}'.format(response.status_code))
+
+
+if __name__ == '__main__':
+    letter = sys.argv[1] if len(sys.argv) > 1 else ''
+    search_user(letter)
