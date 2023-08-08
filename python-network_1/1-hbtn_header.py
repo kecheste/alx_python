cf6cebe5cf8f8b9712d35084b830d815da8ea2cc
kecheste <aben.ezer443@gmail.com>
2023-08-08 23:37:56 +0300

    github api

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -7,7 +7,7 @@ import sys
 
 def get_x_request_id(url):
     """Gets the value of the X-Request-Id header from the response to the given URL."""
-    response = requests.get(url)
+    response = requests.get(str(url))
     return response.headers['X-Request-Id']
 
 
