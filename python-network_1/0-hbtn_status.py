f3f6fd810ab84237a4db3a0f7af990ffb31c0d5b
kecheste <aben.ezer443@gmail.com>
2023-08-08 14:59:23 +0300

    fetch a given url

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -6,16 +6,24 @@ import requests
 
 
 def fetch_url(url):
-    """Fetches a given url
-    arg = url
-    returns response.text    
+    """
+    Fetches the content of a given URL.
+
+    Args:
+        url (str): The URL to fetch.
+
+    Returns:
+        str: The content of the URL.
+
+    Raises:
+        requests.exceptions.RequestException: If an error occurs while making the request.
     """
     response = requests.get(url)
     return response.text
 
 
-if __name__ == '__main__':
-    url = 'https://alu-intranet.hbtn.io/status'
+if __name__ == "__main__":
+    url = "https://alu-intranet.hbtn.io/status"
     body = fetch_url(url)
     print("Body response:")
     print("\t- type:", type(body))
