28092ae26bb04a641a6f9b3fe9f960c9a0b7c748
kecheste <aben.ezer443@gmail.com>
2023-08-10 21:08:43 +0300

    fix

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     letter = "" if len(sys.argv) == 1 else sys.argv[1]
     payload = {"q": letter}
 
-    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    r = requests.post("http://0.0.0.0:5000/search_user", params=payload)
     try:
         response = r.json()
         if response == {}:
