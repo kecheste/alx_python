3c077d11537547adeab5c3f4f27c61c7e54aab7b
kecheste <aben.ezer443@gmail.com>
2023-08-10 03:45:28 +0300

    5-json_api

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,16 +1,23 @@
-"""
-Sends a request to a URL and displays the value of the X-Request-Id header in the response.
-"""
+#!/usr/bin/python3
+"""Fetches the value of the X-Request-Id header from a given URL's response"""
+
 import requests
 import sys
 
 
-def get_x_request_id(url):
-    """Gets the value of the X-Request-Id header from the response to the given URL."""
-    response = requests.get(str(url))
-    return response.headers['X-Request-Id']
+def main():
+    """Main function to fetch and display the X-Request-Id header"""
+    if len(sys.argv) != 2:
+        print("Usage: ./1-hbtn_header.py <URL>")
+        sys.exit(1)
 
-
-if __name__ == '__main__':
     url = sys.argv[1]
-    print(get_x_request_id(url))
+    response = requests.get(url)
+    x_request_id = response.headers.get('X-Request-Id')
+
+    if x_request_id is not None:
+        print(x_request_id)
+
+
+if __name__ == "__main__":
+    main()
