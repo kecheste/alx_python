562d6baaf794198467db293b7ea8e61d4411bc3f
kecheste <aben.ezer443@gmail.com>
2023-08-10 21:44:12 +0300

    fix error code

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
     response = requests.get(url)
 
-    if response.status_code >= 400:
-        print("Error code: ", response.status_code)
+    if (response.status_code >= 400):
+        print("Error code: {}".format(response.status_code))
     else:
         print(response.text)
