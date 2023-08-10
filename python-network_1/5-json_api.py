edf65a5a81a74af48114d54b15e777bdaed21d3c
kecheste <aben.ezer443@gmail.com>
2023-08-10 21:18:58 +0300

    fix

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -9,10 +9,9 @@ import requests
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
-        letter = ""
+        payload = {"q": ''}
     else:
-        letter = sys.argv[1]
-    payload = {"q": letter}
+        payload = {"q": sys.argv[1]}
 
     r = requests.post("http://0.0.0.0:5000/search_user", params=payload)
     try:
