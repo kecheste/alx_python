922800ffbb0019d7bce0ecc2088fb953b3aa6605
kecheste <aben.ezer443@gmail.com>
2023-08-10 20:15:46 +0300

    fix

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,23 +1,28 @@
-#!/usr/bin/python3
-"""Fetches the value of the X-Request-Id header from a given URL's response"""
-
 import requests
 import sys
 
-
-def main():
-    """Main function to fetch and display the X-Request-Id header"""
-    if len(sys.argv) != 2:
-        print("Usage: ./1-hbtn_header.py <URL>")
-        sys.exit(1)
-
-    url = sys.argv[1]
-    response = requests.get(url)
-    x_request_id = response.headers.get('X-Request-Id')
-
-    if x_request_id is not None:
-        print(x_request_id)
-
+# The `get_request_id` function is defined to retrieve the value of the `X-Request-Id` header from a
+# HTTP GET request to a specified URL.
+def get_request_id(*argv):
+    """
+    The function `get_request_id` sends a GET request to a specified URL, retrieves the value of the
+    'X-Request-Id' header from the response, and prints it.
+    """
+    response = requests.get(sys.argv[1])
+    # print(response.headers)
+    # print(response.text)
+    request_id = response.headers.get('X-Request-Id')
+    if request_id:
+        # print(f"The value of X-Request-Id is: {request_id}")
+        print(request_id)
+    __doc__="""doc for class"""
+__doc__="""doc for module"""
 
 if __name__ == "__main__":
-    main()
+    get_request_id(sys.argv)
+
+    # if len(sys.argv) < 2:
+    #     print("Please provide a URL as an argument.")
+    # else:
+    #     # url = sys.argv[1]
+    #     get_request_id(sys.argv)
