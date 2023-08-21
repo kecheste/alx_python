662eb543890a78710df07b4154b8018ef54af4de
kecheste <aben.ezer443@gmail.com>
2023-08-21 08:45:26 +0300

    orm

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,14 @@
 import MySQLdb
 import sys
 
-username = sys.argv[0]
-password = sys.argv[1]
-database = sys.argv[2]
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
 
 db = MySQLdb.connect(
     host='localhost', user=username, passwd=password, db=database, port=3306)
 
 c = db.cursor()
-c.execute("""SELECT * FROM states""")
+c.execute("""SELECT * FROM states ORDER BY states.id ASC""")
+
+c.fetchall()
