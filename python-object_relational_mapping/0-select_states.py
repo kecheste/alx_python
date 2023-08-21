565a1790e027fb21888fd14535f857b731c41ef4
kecheste <aben.ezer443@gmail.com>
2023-08-21 08:38:24 +0300

    orm

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ password = sys.argv[1]
 database = sys.argv[2]
 
 db = MySQLdb.connect(
-    host='localhost', user=username, passwd=password, db=database)
+    host='localhost', user=username, passwd=password, db=database, port=3306)
 
 c = db.cursor()
 c.execute("""SELECT * FROM states""")
