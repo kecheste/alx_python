af9d432d3979ac18a588b8d649be3bbca723e18c
kecheste <aben.ezer443@gmail.com>
2023-08-23 20:49:37 +0300

    all

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,17 +1,34 @@
 #!/usr/bin/python3
 
-# 2. Filter states by user input
+# 3. SQL Injection...
 # mandatory
 
-# Write a script that takes in an argument and displays all values
-# in the states table of hbtn_0e_0_usa where name matches the argument.
+# Wait, do you remember the previous task? Did you test "Arizona';
+# TRUNCATE TABLE states ; SELECT * FROM states WHERE name = '" as an input?
+
+# guillaume@ubuntu:~/$
+# ./2-my_filter_states.py root root hbtn_0e_0_usa "Arizona';
+# TRUNCATE TABLE states ; SELECT * FROM states WHERE name = '"
+# (2, 'Arizona')
+# guillaume@ubuntu:~/$ ./0-select_states.py root root hbtn_0e_0_usa
+# guillaume@ubuntu:~/$
+
+# What? Empty?
+
+# Yes, it’s an SQL injection to delete all records of a table…
+
+# Once again, write a script that takes in arguments and displays
+# all values in the states
+# table of hbtn_0e_0_usa where name matches the argument. But this time,
+# write one that is
+# safe from MySQL injections!
 
 # Your script should take 4 arguments: mysql username, mysql password,
-# database name and state name searched (no argument validation needed)
+# database name
+# and state name searched (safe from MySQL injection)
 # You must use the module MySQLdb (import MySQLdb)
 # Your script should connect to a MySQL server running on
 # localhost at port 3306
-# You must use format to create the SQL query with the user input
 # Results must be sorted in ascending order by states.id
 # Results must be displayed as they are in the example below
 # Your code should not be executed when imported
@@ -25,12 +42,13 @@
 # name VARCHAR(256) NOT NULL,
 # PRIMARY KEY (id)
 # );
-# INSERT INTO states (name) VALUES ("California"), ("Arizona"),
-# ("Texas"), ("New York"), ("Nevada");
+# INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"),
+# ("New York"), ("Nevada");
 
 # guillaume@ubuntu:~/$ cat 0-select_states.sql | mysql -uroot -p
 # Enter password:
-# g:~/$ ./2-my_filter_states.py root root hbtn_0e_0_usa 'Arizona'
+# guillaume@ubuntu:~/$ ./3-my_safe_filter_states.py root
+# root hbtn_0e_0_usa 'Arizona'
 # (2, 'Arizona')
 # guillaume@ubuntu:~/$
 
@@ -40,7 +58,8 @@
 
 """takes in an argument and displays all values
 in the states table of hbtn_0e_0_usa
-where name matches the argument"""
+where name matches the argument
+and is safe from SQL injections"""
 
 if __name__ == '__main__':
 
@@ -51,8 +70,8 @@ if __name__ == '__main__':
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
-                ORDER BY states.id ASC".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE name=%s\
+                ORDER BY states.id ASC", (sys.argv[4],))
     rows = cur.fetchall()
     for row in rows:
         print(row)
---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# 8-model_state_fetch_first.py
+"""import a model and fitch the first row"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format
+        (argv[1],
+         argv[2],
+         argv[3]),
+        pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    if session.query(State).count() == 0:
+        print("Nothing")
+
+    state = session.query(State).first()
+
+    print('{}: {}'.format(state.id, state.name))
+    session.close()
