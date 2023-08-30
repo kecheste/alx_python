3830d9554f6fe01f82466592fdfd46cdf3fd020c
kecheste <aben.ezer443@gmail.com>
2023-08-30 16:14:44 +0300

    default route

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -1,7 +1,7 @@
-"""
-Starts a Flask web application.
-"""
-
+#!/usr/bin/python3
+'''
+   Starts a Flask web application.
+'''
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,27 +9,23 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!' when accessing root route."""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    "Displays 'HBNB! when accessing /hbnb route."
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    """display “C ” followed by the value of the text"""
+def display_c_text(text):
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def c_text(text):
-    """display “Python ” followed by the value of the text"""
+def display_python_text(text):
     text = text.replace('_', ' ')
     return 'Python {}'.format(text)
 
