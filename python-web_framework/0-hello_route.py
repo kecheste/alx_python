3cdd14ff72ef1421e3b0ac9a9e80af3e9fc95c88
kecheste <aben.ezer443@gmail.com>
2023-08-30 16:03:09 +0300

    fix

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,18 +1,17 @@
-# /usr/bin/python3
+"""
+Starts a Flask web application.
+"""
+
 from flask import Flask
-# importing flask module into my app
 
 app = Flask(__name__)
-
-# Define a route for the root URL with strict_slashes=False
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Displays 'Hello HBNB!' when accessing root route."""
     return 'Hello HBNB!'
-# defining basic routing
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-# running the app
