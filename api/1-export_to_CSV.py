5d271caa78df7c1f9b09337aa2cda58e1e247cdc
kecheste <aben.ezer443@gmail.com>
2023-10-04 18:22:32 +0300

    all

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,30 +1,45 @@
 #!/usr/bin/python3
 """
-Exporitng the data from the api requests to csv file
+Python script to export data in the CSV format.
 """
+
 import csv
 import requests
 import sys
 
+
+def export_to_CSV(user_id):
+    employee_name = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+    ).json()["name"]
+    tasks = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id)
+    ).json()
+
+    tasks_data = []
+
+    for task in tasks:
+        tasks_data.append(
+            [
+                str(user_id),
+                employee_name,
+                task["completed"],
+                task["title"],
+            ]
+        )
+
+    with open(str(user_id) + ".csv", "w", encoding="UTF8", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(tasks_data)
+
+
 if __name__ == "__main__":
-    user_id = sys.argv[1]
+    if len(sys.argv) != 2:
+        print("Usage: python3 script_name.py EMPLOYEE_ID")
+        sys.exit(1)
 
-    # getting user
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-    user = requests.get(user_url)
-    user_data = user.json()
-
-    # getting todos
-    todo_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
-        user_id)
-    todos = requests.get(todo_url)
-    todos_data = todos.json()
-
-    # exporting the data to csv
-    csv_file = "{}.csv".format(user_id)
-
-    with open(csv_file, 'w') as csvfile:
-        csv_writer = csv.writer(csvfile)
-        for task in todos_data:
-            csv_writer.writerow(
-                [user_id, user_data['name'], task['completed'], task['title']])
+    try:
+        employee_id = int(sys.argv[1])
+        export_to_CSV(employee_id)
+    except ValueError:
+        print("Please provide a valid employee ID.")
